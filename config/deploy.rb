484c134e1fa4a42cc264c87c43e73f3a1143c994
mmkserver <black.rey.2@gmail.com>
2014-06-27 15:34:54 +0400

    initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,17 @@
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+require 'capistrano'
+require 'bundler/capistrano'
+
+set :stages, %w(staging production)
+set :default_stage, "production"
+require 'capistrano/ext/multistage'
+
+set :application, "health"
+
+set :scm, :git
+set :repository,  "git@github.com:mmkserver/mmksrv.git"
+set :deploy_via, :copy
+set :copy_strategy, :export
+
+set :user, 'deploy'
+set :use_sudo, false
